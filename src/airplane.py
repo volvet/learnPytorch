f556abed3d8a202eaf5bc32c287a9027b3244b8d
Volvet Zhang <volvet@163.com>
2021-01-21 20:28:27 +0800

    airplane

---
+++ src/airplane.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 21 17:44:07 2021
+
+@author: volvetzhang
+"""
+
+import torch
+from torchvision import datasets, transforms
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def airplane():
+  pass
+
+if __name__ == '__main__':
+  torch.manual_seed(123)
+  airplane()
